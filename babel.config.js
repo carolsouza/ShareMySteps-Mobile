4355ccc54f2381f110bb60dfe79b6edc79306352
carolsouza <caah.souza@live.com>
2022-03-03 23:41:12 -0300

    Ajustes na env

--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['react-native-reanimated/plugin'],
-    ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
+    plugins: [["module:react-native-dotenv", {
       "moduleName": "@env",
       "path": ".env",
-      "blocklist": null,
-      "allowlist": null,
-      "blacklist": null, // DEPRECATED
-      "whitelist": null, // DEPRECATED
-      "safe": false,
-      "allowUndefined": true,
-      "verbose": false
-    }]]
+      "blacklist": null,
+      "whitelist": null,
+      "safe": true,
+      "allowUndefined": true
+      }]
+      ]
+  
   };
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import  { API_BASE_URL }  from '@env';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_ENDPOINT
+    baseURL: API_BASE_URL
 })
 
 export default api;
